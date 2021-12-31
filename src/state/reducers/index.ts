83ea716bddd837790988cb2c0c4d9c383e8d8dc7
Jake Harris <milieu@javakat.io>
2021-12-31 13:20:30 -0600

    Allow root state type to be imported

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,7 +1,7 @@
 import { createSelector } from "@reduxjs/toolkit";
 import * as initialSceneState from "./initial-scene.state";
 
-type State = {
+export type State = {
   initialScene: initialSceneState.State;
 };
 
