e5e6c74a33beed84d95bc5c0a1a3cb447770f664
Jake Harris <milieu@javakat.io>
2022-01-10 20:59:54 -0600

    Add unit model, helper functions

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from "./consts";
 export * from "./positioned";
 export * from "./tile";
+export * from "./unit";
---
+++ src/models/unit.ts
@@ -0,0 +1,97 @@
+export interface Unit {
+  id: string;
+
+  name: string;
+  x: number;
+  y: number;
+
+  team: Team;
+
+  hp: number;
+  maxHp: number;
+  attack: number;
+  defense: number;
+  speed: number;
+  range: number;
+}
+
+export const enum Team {
+  Player,
+  Enemy,
+  Ally,
+  Other,
+}
+
+export function getTeamColor(team: Team) {
+  switch (team) {
+    case Team.Player:
+      return 0x0000ff;
+    case Team.Enemy:
+      return 0xff0000;
+    case Team.Ally:
+      return 0x00ff00;
+    case Team.Other:
+      return 0xffff00;
+  }
+}
+
+export function createUnit(updates?: Partial<Unit>): Unit {
+  return {
+    id: Math.random() * 1e16 + "",
+    name: "Absolute Unit",
+    x: 0,
+    y: 0,
+    team: Team.Player,
+    hp: 10,
+    maxHp: 10,
+    attack: 1,
+    defense: 1,
+    speed: 1,
+    range: 1,
+    ...updates,
+  };
+}
+
+export function createRandomBasicUnit(
+  mapWidth: number,
+  mapHeight: number,
+  updates?: Partial<Unit>
+): Unit {
+  const x = Math.floor(Math.random() * mapWidth);
+  const y = Math.floor(Math.random() * mapHeight);
+  const team = Math.floor(Math.random() * 4) as Team;
+
+  return createUnit({
+    x,
+    y,
+    team,
+    ...updates,
+  });
+}
+
+export function createRandomBasicUnits(
+  unitCount: number,
+  mapWidth: number,
+  mapHeightNumber: number
+) {
+  const units = [];
+  for (let i = 0; i < unitCount; i++) {
+    units.push(createRandomBasicUnit(mapWidth, mapHeightNumber));
+  }
+  return units;
+}
+
+export function createRandomInitialUnits(
+  unitCount: number,
+  mapWidth: number,
+  mapHeightNumber: number
+) {
+  const units = [];
+  units.push(
+    createRandomBasicUnit(mapWidth, mapHeightNumber, { team: Team.Player })
+  );
+  for (let i = 0; i < unitCount - 1; i++) {
+    units.push(createRandomBasicUnit(mapWidth, mapHeightNumber));
+  }
+  return units;
+}
