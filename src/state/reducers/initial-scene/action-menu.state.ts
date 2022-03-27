e26fc3e1cad184b707beb98bf8059f69f8a0316d
Jake Harris <milieu@javakat.io>
2022-03-27 16:00:48 -0500

    Extract action-menu-options to models

--- src/state/reducers/initial-scene/action-menu.state.ts
+++ src/state/reducers/initial-scene/action-menu.state.ts
@@ -1,18 +1,5 @@
 import { Action, createSlice, PayloadAction } from "@reduxjs/toolkit";
-
-export enum ActionMenuOptions {
-  Attack,
-  Wait,
-}
-
-export function getActionName(action: ActionMenuOptions) {
-  switch (action) {
-    case ActionMenuOptions.Attack:
-      return "Attack";
-    case ActionMenuOptions.Wait:
-      return "Wait";
-  }
-}
+import { ActionMenuOptions } from "../../../models/action-menu-options";
 
 export interface State {
   cursorIndex: number;
